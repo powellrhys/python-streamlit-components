ac7f32e51cd695b9cf6cc7f22bead8aade2e5c8c
rhys powell <rhys.powell2@btinternet.com>
2025-04-15 17:24:40 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='streamlit-components',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit==1.44.0'
+    ],
+    author='Rhys Powell',
+    description='A package that defines reuseable streamlit components',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/powelrhys/python-streamlit-components',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
